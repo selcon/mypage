5d32bd73565bf792be715ee4dbe3e02e5a456aa3
kottn <35750723+kottn@users.noreply.github.com>
2018-04-02 19:57:03 +0900

    rebuilding site 2018年  4月  2日 月曜日 19:57:03 JST

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,9 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
+rm -rf public docs
+hugo
+cp -r public docs
 git add .
 
 # Commit changes.
@@ -15,10 +12,7 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
+git commit -m "$msg"
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
--- flush.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#rm -rf public/*
-#echo "gitdir: ../.git/modules/public" > public/.git
