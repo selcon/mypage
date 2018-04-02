b8f5ae522ab50d1e55610e60ff76a9d5dec25334
kottn <35750723+kottn@users.noreply.github.com>
2018-04-02 20:17:43 +0900

    rebuilding site 2018年  4月  2日 月曜日 20:17:43 JST

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+MYPAGE=mypage
+ID=kottn
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-rm -rf public docs
+rm -rf public
 hugo
-cp -r public docs
 git add .
 
-# Commit changes.
+# Commit changes. for "${MYPAGE}"
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
@@ -15,4 +17,14 @@ fi
 
 git commit -m "$msg"
 git push origin master
+
+# Commit changes. for "${ID}.github.io"
+cd ..
+git clone https://github.com/${ID}/${ID}.github.io
+cd ${ID}.github.io && rm -rf * && cp -r ../${MYPAGE}/public/* .
+git add .
+git commit -m "$msg"
+git push origin master
+
+cd ../${MYPAGE}
 
