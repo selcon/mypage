f1e777cc174f6c7ec851724b987fc28bd31ad2db
kottn <35750723+kottn@users.noreply.github.com>
2018-04-05 18:32:18 +0900

    mod filename

--- finished.sh
+++ finished.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DRAFT=$(\grep -rn "draft: false" ./content | tr ':' ' ' | awk '{print $1}' | nl -w 4)
 echo ""
-echo -e "<No.>\t<Draft-file>"
+echo -e "<No.>\t<Finished file>"
 echo "${DRAFT}"
 echo ""
 echo "Open No.?"
