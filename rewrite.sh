3f372ef50d9bd2d663bda30c40998e76e73b0291
kottn <35750723+kottn@users.noreply.github.com>
2018-05-09 15:29:00 +0900

    fix for ls

--- rewrite.sh
+++ rewrite.sh
@@ -3,21 +3,21 @@ echo ""
 echo "*** You can check ALL posts and notes with '-f' option"
 
 if [[ $1 == "-f" ]]; then
-    DRAFT=$(\grep -ilrn "draft: false" ./content | xargs \ls -lt | awk '{print $9}' | nl -w 4)
+    DRAFT=$(\grep -ilrn "draft: false" ./content |xargs \ls -lt *.md |grep -v "README.md" |awk '{print $9}' |nl -w 4)
     echo ""
     echo -e "<No.>\t<Finished file>"
     echo "${DRAFT}"
     echo ""
     echo "Open No.?"
     read ANS
-    vim $(echo "${DRAFT}" | awk "NR==$ANS" | awk '{print $2}')
+    vim $(echo "${DRAFT}" |awk "NR==$ANS" |awk '{print $2}')
 else
-    DRAFT=$(\grep -ilrn "draft: true" ./content | xargs \ls -lt | awk '{print $9}' | nl -w 4)
+    DRAFT=$(\grep -ilrn "draft: true" ./content |xargs \ls -lt *.md |grep -v "README.md" |awk '{print $9}' |nl -w 4)
     echo ""
     echo -e "<No.>\t<Draft file>"
     echo "${DRAFT}"
     echo ""
     echo "Open No.?"
     read ANS
-    vim $(echo "${DRAFT}" | awk "NR==$ANS" | awk '{print $2}')
+    vim $(echo "${DRAFT}" |awk "NR==$ANS" |awk '{print $2}')
 fi
